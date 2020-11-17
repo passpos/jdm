86ec33eab4847ae4334353df8be1ead19166e467
passpos <passpos@outlook.com>
2020-11-17 21:49:10 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

---
+++ jdm-common/jdm-launcher/src/main/java/jdm/launcher/Main.java
@@ -0,0 +1,35 @@
+package jdm.launcher;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//import jdm.lang.ComparableDemo;
+//import jdm.lang.StringBuilderDemo;
+//import jdm.lang.SystemDemo;
+//import jdm.lang.samples.thread.ByExtendsThread;
+//import jdm.lang.samples.thread.ByImplRunnable;
+//import jdm.base.io.FileDemo;
+import jdm.base.util.function.ConsumerDemo;
+//import jdm.repo.demo.algorithm.RecursionDemo;
+//import jdm.repo.entity.Teacher;
+//import jdm.samples.stums.StudentManager;
+//import text.SimDateFormateDemo;
+//import jdm.util.ArrayListDemo;
+//import jdm.util.FileCollectionDemo;
+//import jdm.util.RandomDemo;
+
+/**
+ *
+ * @author realpai <paiap@outlook.com>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ConsumerDemo demo = new ConsumerDemo();
+        demo.show();
+    }
+
+}
