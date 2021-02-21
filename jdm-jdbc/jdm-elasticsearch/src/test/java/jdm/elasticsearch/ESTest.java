746ef495d22084caf0dfdb466037fcf85f88d124
passpos <passpos@outlook.com>
2021-02-21 13:43:49 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jdm-jdbc/jdm-elasticsearch/src/test/java/jdm/elasticsearch/ESTest.java
+++ jdm-jdbc/jdm-elasticsearch/src/test/java/jdm/elasticsearch/ESTest.java
@@ -10,7 +10,12 @@ import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -28,20 +33,62 @@ public class ESTest {
         System.out.println("testConnect() - 连接成功");
     }
 
+    @Test
+    public void createIndex() throws IOException {
+        if (testExist()) {
+            System.out.println("createIndex() - 已经存在索引：" + index);
+            return;
+        }
+        // 1. 准备关于索引的settings
+        Settings.Builder settings = Settings.builder()
+                .put("number_of_shards", 3)
+                .put("number_of_replicas", 1);
+
+        // 2. 映射数据结构
+        XContentBuilder mappings = JsonXContent.contentBuilder();
+
+        mappings.startObject().startObject("properties")
+                .startObject("name")
+                .field("type", "text")
+                .endObject()
+                .startObject("age")
+                .field("type", "integer")
+                .endObject()
+                .startObject("birthday")
+                .field("type", "date")
+                .field("format", "yyyy-MM-dd")
+                .endObject()
+                .endObject().endObject();
+
+        // 3. 将settings 与 mappings 封装到一个Request对象；
+        CreateIndexRequest ciRequest = new CreateIndexRequest(index);
+        ciRequest.settings(settings).mapping(mappings);
+
+        // 4. 通过client对象，去连接ES并执行创建索引
+        CreateIndexResponse resp;
+
+        resp = client.indices().create(ciRequest, RequestOptions.DEFAULT);
+
+        // 5. 输出
+        System.out.println("创建索引 - " + resp.toString());
+    }
+
     /**
      * 判断索引是否存在
+     * @return
      * @throws IOException
      */
     @Test
-    public void testExist() throws IOException {
+    public boolean testExist() throws IOException {
         // 1. 准备request对象
         GetIndexRequest iRequest = new GetIndexRequest(index);
 
         // 2. 通过client去操作
         boolean exists = client.indices().exists(iRequest, RequestOptions.DEFAULT);
 
-        // 3. 输出
+        // 3. 输出结果
         System.out.println("testExist() - " + exists);
+        return exists;
     }
 
     /**
@@ -56,7 +103,16 @@ public class ESTest {
         // 2. 通过client去操作
         AcknowledgedResponse delete = client.indices().delete(diRequest, RequestOptions.DEFAULT);
 
-        // 3. 输出
+        // 3. 输出结果
         System.out.println("testDelete() - " + delete.isAcknowledged());
+    }
+
+    @Test
+    public void testCreateDoc() {
+        // 1. 准备一个json数据
+
+        // 2. 准备request对象
+        // 3. 通过client去操作
+        // 4. 输出结果
     }
 }
