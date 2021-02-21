569b05c573b87a64c4e122c1969f3314107d59a4
passpos <passpos@outlook.com>
2021-02-21 12:39:44 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jdm-jdbc/jdm-elasticsearch/src/test/java/jdm/elasticsearch/ESTest.java
+++ jdm-jdbc/jdm-elasticsearch/src/test/java/jdm/elasticsearch/ESTest.java
@@ -5,8 +5,10 @@ package jdm.elasticsearch;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.IOException;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -15,9 +17,24 @@ import org.junit.jupiter.api.Test;
  */
 public class ESTest {
 
+    public RestHighLevelClient client = ESClient.getClient();
+    public String index = "man";
+
     @Test
     public void testConnect() {
         RestHighLevelClient client = ESClient.getClient();
-        System.out.println("OK - 连接成功");
+        System.out.println("testConnect() - 连接成功");
+    }
+
+    @Test
+    public void testExist() throws IOException {
+        // 1. 准备request对象
+        GetIndexRequest iRequest = new GetIndexRequest(index);
+
+        // 2. 通过client去操作
+        boolean exists = client.indices().exists(iRequest, RequestOptions.DEFAULT);
+
+        // 3. 输出
+        System.out.println("testExist() - " + exists);
     }
 }
