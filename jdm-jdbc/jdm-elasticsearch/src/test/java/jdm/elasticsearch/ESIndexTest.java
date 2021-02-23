3808c765af6705ffb135bf17b3fbfced9056fd2e
passpos <passpos@outlook.com>
2021-02-23 21:36:00 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jdm-jdbc/jdm-elasticsearch/src/test/java/jdm/elasticsearch/ESIndexTest.java
+++ jdm-jdbc/jdm-elasticsearch/src/test/java/jdm/elasticsearch/ESIndexTest.java
@@ -5,19 +5,9 @@ package jdm.elasticsearch;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.action.delete.DeleteRequest;
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
@@ -25,7 +15,6 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.junit.jupiter.api.Test;
 
@@ -33,11 +22,10 @@ import org.junit.jupiter.api.Test;
  *
  * @author passpos <paiap@outlook.com>
  */
-public class ESTest {
+public class ESIndexTest {
 
     public RestHighLevelClient client = ESClient.getClient();
     public String index = "person";
-    public ObjectMapper mapper = new ObjectMapper();
 
     @Test
     public void testConnect() {
@@ -122,53 +110,6 @@ public class ESTest {
 
         // 3. 输出结果
         System.out.println("testDelete() - " + delete.isAcknowledged());
-    }
-
-    public void testCreateDoc() throws JsonProcessingException, IOException {
-        // 1. 准备一个json数据
-        Person person = new Person(3, "童柏雄", 39, new Date());
-        String json = mapper.writeValueAsString(person);
-
-        // 2. 准备 request 对象
-        IndexRequest iRequest = new IndexRequest(index);
-        iRequest.id(String.valueOf(person.getId()));
-        iRequest.source(json, XContentType.JSON);
-
-        // 3. 通过client去操作
-        IndexResponse iResp = client.index(iRequest, RequestOptions.DEFAULT);
-
-        // 4. 输出结果
-        System.out.println("testCreateDoc() - " + iResp.getResult().toString());;
-    }
-
-    @Test
-    public void testUpdateDoc() throws IOException {
-        // 1. 创建一个Map，指定需要修改的内容
-        HashMap<String, Object> doc = new HashMap<>();
-        doc.put("age", "47");
-        String docId = "1";
-
-        // 2. 创建 request 对象，封装数据
-        UpdateRequest uReq = new UpdateRequest(index, docId);
-        uReq.doc(doc);
-
-        // 3. 通过 client 执行
-        UpdateResponse update = client.update(uReq, RequestOptions.DEFAULT);
-
-        // 4. 输出结果
-        System.out.println("testUpdateDoc() - " + update.getResult().toString());
-    }
-
-    @Test
-    public void testDeleteDoc() throws IOException {
-        // 1. 创建 request 对象
-        DeleteRequest dReq = new DeleteRequest(index, "3");
-
-        // 2. 通过 client 执行
-        DeleteResponse delete = client.delete(dReq, RequestOptions.DEFAULT);
-
-        // 3. 输出结果
-        System.out.println("testDeleteDoc() - " + delete.getResult().toString());
     }
 
 }
