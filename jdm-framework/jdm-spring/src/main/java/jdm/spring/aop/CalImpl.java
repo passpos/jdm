98673eaae963df80a2358e44317d786ff3f0d01d
passpos <passpos@outlook.com>
2021-03-03 17:18:13 +0800

    update Signed-off-by:passpos <passpos@outlook.com>

--- jdm-framework/jdm-spring/src/main/java/jdm/spring/aop/CalImpl.java
+++ jdm-framework/jdm-spring/src/main/java/jdm/spring/aop/CalImpl.java
@@ -13,26 +13,22 @@ public class CalImpl implements Calcuator {
 
     @Override
     public int add(int num1, int num2) {
-        int res = num1 + num2;
-        return res;
+        return num1 + num2;
     }
 
     @Override
     public int sub(int num1, int num2) {
-        int res = num1 - num2;
-        return res;
+        return num1 - num2;
     }
 
     @Override
     public int mul(int num1, int num2) {
-        int res = num1 * num2;
-        return res;
+        return num1 * num2;
     }
 
     @Override
     public int div(int num1, int num2) {
-        int res = num1 / num2;
-        return res;
+        return num1 / num2;
     }
 
 }
